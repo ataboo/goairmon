417f3e92b21eb4e5c8a1fe55f279bbca550d2dfb
Alex Raboud <doubar2001@gmail.com>
2019-09-17 23:49:45 -0600

    added arm dist

--- site/controllers/homecontroller.go
+++ site/controllers/homecontroller.go
@@ -1,10 +1,7 @@
 package controllers
 
 import (
-	"fmt"
-	"goairmon/business/data/context"
 	"goairmon/business/services/identity"
-	"goairmon/site/helper"
 	"goairmon/site/models"
 
 	"github.com/labstack/echo"
@@ -15,13 +12,7 @@ func HomeController(server *echo.Echo, identity *identity.IdentityService) *echo
 	group.GET("/", func(c echo.Context) error {
 		view := loadView("home/index.gohtml", c)
 
-		dbContext := c.Get(helper.CtxDbContext).(context.DbContext)
-		points, err := dbContext.GetSensorPoints(24 * 60)
-		if err != nil {
-			return fmt.Errorf("failed to get sensor points", err)
-		}
-
-		return view.Execute(c.Response().Writer, models.NewContextVm(c, models.GraphVm{SensorPoints: points}))
+		return view.Execute(c.Response().Writer, models.NewContextVm(c, nil))
 	}, identity.RedirectUsersWithoutSession("/auth/login"))
 
 	return group
